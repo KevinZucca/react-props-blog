445afaa8cc8ce5c28862f6a785575b9793a5e2c8
KevinZucca <kev.94@live.it>
2023-11-30 17:37:50 +0100

    feat: condition in empty json case

--- src/components/TheMain.jsx
+++ src/components/TheMain.jsx
@@ -10,25 +10,28 @@ export default function TheMain() {
         <div className="mx-auto max-w-2xl px-4 pb-20 sm:px-6 lg:max-w-7xl lg:px-8">
           <TagsList />
           <Article />
-
           <div className="mt-6 grid grid-cols-1 gap-y-10 overflow-hidden sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {postsList.map((post, i) => (
-              <div
-                key={i}
-                className={`group relative ${
-                  post.published === false && 'hidden'
-                }`}
-              >
-                <Post
-                  id={post.id}
-                  title={post.title}
-                  image={post.image}
-                  content={post.content}
-                  tags={post.tags}
-                  published={post.published}
-                ></Post>
-              </div>
-            ))}
+            {postsList.length > 0 ? (
+              postsList.map((post, i) => (
+                <div
+                  key={i}
+                  className={`group relative ${
+                    post.published === false && 'hidden'
+                  }`}
+                >
+                  <Post
+                    id={post.id}
+                    title={post.title}
+                    image={post.image}
+                    content={post.content}
+                    tags={post.tags}
+                    published={post.published}
+                  ></Post>
+                </div>
+              ))
+            ) : (
+              <h2>No posts available</h2>
+            )}
           </div>
         </div>
       </div>
